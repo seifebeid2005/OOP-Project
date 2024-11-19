5a7960e0eb093a19b75cda362fefa40d1a0b8523
seifebeid2005 <seif.amr.ebeid@gmail.com>
2024-11-19 20:27:43 +0200

    date changes

--- src/classes/Admin/Admin.java
+++ src/classes/Admin/Admin.java
@@ -1,10 +1,11 @@
 package classes.Admin;
 import classes.person.Person;
+import java.util.Date;
 public class Admin extends Person {
 
     private String role;
 
-    public Admin(int id, String name, int age, String email, int schoolID, int DateAdded, String role) {
+    public Admin(int id, String name, int age, String email, int schoolID, Date DateAdded, String role) {
         super(id, name, age, email, DateAdded);
         this.role = role;
     }
