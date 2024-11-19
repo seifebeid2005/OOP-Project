6d89a92901189af8cd602ec9fe05ad6051d3cf1b
JohnRamez06 <J.Labib54095@student.aast.edu>
2024-11-19 20:55:57 +0200

    Update tutor class

---
+++ src/classes/Tutor/Tutor.java
@@ -0,0 +1,13 @@
+
+public class Tutor {
+    private int tutor_ID;
+    private String tutor_name;
+    private String tutor_email;
+    private String tutor_password;
+    private String tutor_phone;
+    private String tutor_address;
+    private String tutor_qualification;
+    private String tutor_experience;
+    private String tutor_subject;
+
+}
