b6b644faf6d30732a9be1d0f938bdac88ecdaa2c
Nagy2811 <boudy2811@gmail.com>
2024-11-21 10:34:51 +0200

    add classes Achievement and CourseLevel

--- src/classes/Achievement/Achievement.java
+++ src/classes/Achievement/Achievement.java
@@ -1,5 +1,70 @@
 package classes.Achievement;
+import java.time.LocalDateTime;
 
 public class Achievement {
-    
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDateTime dateAchieved;
+    private Integer studentID;
+
+    public Achievement(Integer id, String title, String description, LocalDateTime dateAchieved, Integer studentID) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dateAchieved = dateAchieved;
+        this.studentID = studentID;
+    }
+
+    public Integer getID() {
+        return id;
+    }
+
+    public void setID(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateAchieved() {
+        return dateAchieved;
+    }
+
+    public void setDateAchieved(LocalDateTime dateAchieved) {
+        this.dateAchieved = dateAchieved;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Integer studentID) {
+        this.studentID = studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Achievement {" +
+               "id=" + id +
+               ", title='" + title + '\'' +
+               ", description='" + description + '\'' +
+               ", dateAchieved=" + dateAchieved +
+               ", studentID=" + studentID +
+               '}';
+    }
 }
+
--- src/classes/CourseLevel/CourseLevel.java
+++ src/classes/CourseLevel/CourseLevel.java
@@ -1,6 +1,69 @@
 package classes.CourseLevel;
 
 public class CourseLevel {
-    // var
-    
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer requiredProgress;
+    private Boolean isActive;
+
+    public CourseLevel(Integer id, String name, String description, Integer requiredProgress, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.requiredProgress = requiredProgress;
+        this.isActive = isActive;
+    }
+
+    public Integer getID() {
+        return id;
+    }
+
+    public void setID(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRequiredProgress() {
+        return requiredProgress;
+    }
+
+    public void setRequiredProgress(Integer requiredProgress) {
+        this.requiredProgress = requiredProgress;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseLevel {" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", description='" + description + '\'' +
+               ", requiredProgress=" + requiredProgress +
+               ", isActive=" + isActive +
+               '}';
+    }
 }
+
